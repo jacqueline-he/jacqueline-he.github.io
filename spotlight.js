229e97404ac029720cbc379312cb265048e76dbe
jacqueline-he <jacquelinehe00@gmail.com>
2019-07-05 01:32:10 -0700

    Add spotlight effect
    Black looks good, sky blue too (@ 60% radius, may need further tweaking)

---
+++ spotlight.js
@@ -0,0 +1,8 @@
+var img=$('.cont');
+$(document).ready(function(e) {
+    $(document).mousemove(function(e) {
+        var positionLeft = e.clientX - img.width()/2;
+        var positionTop = e.clientY - img.height()/2;
+        img.css({'position': 'fixed', 'left': positionLeft, 'top': positionTop});
+    });
+});
